f5b98f9de7943b43902ef4a14046a2c50be7eeb8
hrcn <hrcn1128@gmail.com>
2020-07-09 16:47:13 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import { Cards, Chart, CountryPicker } from './components'
+import styles from './App.module.css'
+
+export class App extends Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
+      </div>
+    )
+  }
+}
+
+export default App
